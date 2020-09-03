6d18114bfd07ebd6e5e8da5d27e31ca5f12c85de
Shubh Nisar <65038837+Shubh-Nisar@users.noreply.github.com>
2020-09-03 16:42:36 +0530

    Local Updates

---
+++ unicode-learning-period/task-one/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Input from '../src/components/Input/Input';
+
+function App() {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
+
+export default App;
