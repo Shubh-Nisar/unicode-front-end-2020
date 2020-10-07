8904547c4982b94cb785a191051a95bc7cea68b4
Shubh Nisar <65038837+Shubh-Nisar@users.noreply.github.com>
2020-10-07 22:05:28 +0530

    Recursive Routing and Charts

--- unicode-learning-period/task-three/src/pages/Country/Country.js
+++ unicode-learning-period/task-three/src/pages/Country/Country.js
@@ -1,15 +1,22 @@
-import { isInaccessible } from '@testing-library/react';
 import React, { useState, useEffect } from 'react';
+import '../Country/Country.css';
+
+import Border from '../../components/Border/Border';
 
 import { GoogleLogin } from 'react-google-login';
+import { Avatar, Chip } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
+import { Link } from 'react-router-dom';
 const axios = require('axios');
 
 const Country = props => {
+    const totalPopulation = localStorage.getItem('totalPopulation');
+    console.log(totalPopulation);
     const countryName = window.location.href.split('/')[3];
     console.log(countryName);
     const [countryData, setCountryData] = useState();
     const fetchCountryData = () => {
-        const countryUrl = `https://restcountries.eu/rest/v2/name/${countryName}?fullText=true`;
+        const countryUrl = `https://restcountries.eu/rest/v2/alpha/${countryName}`;
         return axios(countryUrl)
             .then(res => {
                 console.log(res);
@@ -20,21 +27,78 @@ const Country = props => {
         fetchCountryData();
     }, [countryName]);
 
+    const DoughnutChart = () => {
+        const data = {
+            datasets: [{
+                data: [totalPopulation, countryData.data.population],
+                backgroundColor: ['rgba(11,181,255, 0.3)', 'rgba(255, 85, 0, 0.8)']
+            }],
+        
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Current World Population',
+                `Current ${countryData.data.name} Population`
+            ],
+        };
+        return (
+            <div className="CountryDoughnut-block">
+                <p>Population Analysis</p>
+                <div>
+                <div>
+                    <Doughnut 
+                    data={data}
+                    />
+                </div>
+                <p style={{fontSize: '1.5vw', textAlign: 'left'}}>
+                    {countryData.data.name} [{countryData.data.nativeName}] lying in {countryData.data.region}<br />comprises 
+                    <span style={{fontWeight: 'bolder'}}>  {(countryData.data.population*100/totalPopulation).toFixed(2)}%</span><br />of World Population.
+                </p>
+                </div>
+            </div>
+        );
+    };
+
+    const borderCountry = () => {
+        const borderArray = [];
+        countryData.data.borders.map(elem => borderArray.push(`${elem};`));
+        console.log(borderArray);
+        let borderCountryUrl = `https://restcountries.eu/rest/v2/alpha?codes=${borderArray.map(elem => elem)}`;
+        borderCountryUrl = borderCountryUrl.replace(/,/g, '');
+        console.log(borderCountryUrl);
+        localStorage.setItem('borderCountryUrl', borderCountryUrl);
+        return (
+            <Border borderCountryUrl={borderCountryUrl} country={countryData.data.name}/>
+        );
+    };
+
     if(localStorage.getItem('loginTask3') === '0'){
         return (
-            <div style={{width: '100vw', height: '100vh', backgroundColor: 'rgba(0, 0, 0, 0.75)'}}>
+            <div style={{width: '100vw', height: '100vh', backgroundColor: 'rgba(0, 0, 0, 0.75)', overflowX: 'hidden'}}>
                 <p style={{paddingTop: '50vh', color: 'white'}}>Please Login to access this part of the page!</p>
             </div>
         );
     }
+
+
+
     return (
         <div className="Country-block">
             {console.log(localStorage.getItem('loginTask3'))}
             {countryData && window.localStorage.getItem("loginTask3") && (<div className="Country-block__elem--countryDetails">
-            <p style={{fontSize: 100}}>{countryData.name}</p>
-            <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+            <div className="Country-block__elem--countryContainer">
+                <p className="Country-block__elem--countryName">{countryData.data.name} <img src={countryData.data.flag} width={100} style={{verticalAlign: 'middle', marginBottom: 10, borderRadius: 8, border: 'black 2px solid'}}/></p>
+                <p className="Country-block__elem--countryCapital">{countryData.data.capital} | {countryData.data.timezones[0]}</p>
+                <p className="Country-block__elem--countryPopulation">{countryData.data.population.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1,")}</p>
+                <p>{countryData.data.borders.map(elem => <Chip 
+                key={elem} label={elem} 
+                style={{margin: 5, fontSize: 20, backgroundColor: '#9F9F9D'}}
+                component={Link}
+                to={`/${elem}`}
+                ></Chip>)}</p>
+                <hr />
+            </div>
+            {totalPopulation && (DoughnutChart())}
+            {borderCountry()}
             </div>)}
         </div>
     );
--- unicode-learning-period/task-three/src/pages/Home/Home.js
+++ unicode-learning-period/task-three/src/pages/Home/Home.js
@@ -7,12 +7,16 @@ const axios = require('axios');
 
 const Home = props => {
     const [data, setData] = useState();
+    let totalPopulation = 0;
     const fetchWorldData = () => {
         const worldUrl = 'https://restcountries.eu/rest/v2/all';
         return axios(worldUrl)
             .then(res => {
                 console.log(res.data.sort((a,b) => a-b));
                 setData(res.data.sort((a,b) => b.population-a.population));
+                res.data.map(elem => totalPopulation += elem.population);
+                console.log(totalPopulation.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1,"));
+                localStorage.setItem('totalPopulation', totalPopulation);
             })
             .catch(err => console.log(err));
     };
@@ -26,7 +30,7 @@ const Home = props => {
         <div className='Home-block'>
             <TableContainer component={Paper}>
             <Table aria-label="simple table" style={{minWidth: 650}}>
-            <TableHead>
+            <TableHead style={{backgroundColor: '#eee', borderBottom: '2px #333 solid'}}>
                 <TableRow>
                 <TableCell component="th" scope="row">Name</TableCell>
                 <TableCell align="right">Population</TableCell>
@@ -36,9 +40,9 @@ const Home = props => {
             </TableHead>
             <TableBody>
             {data && (
-                data.map(elem => <TableRow key={elem.population} component={Link} to={`/${elem.name}`}>
+                data.map(elem => <TableRow key={elem.population} component={Link} to={`/${elem.alpha3Code}`}>
                     <TableCell component="th" scope="row">{elem.name}</TableCell>
-                    <TableCell align="right">{elem.population}</TableCell>
+                    <TableCell align="right">{elem.population.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1,")}</TableCell>
                     <TableCell align="right">{elem.capital}</TableCell>
                     <TableCell align="right">{elem.currencies[0].symbol} {elem.currencies[0].name}</TableCell>
                 </TableRow>)
